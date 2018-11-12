5b08feb97be2cd9de4f3c3b211ac0c59e1962695
compsysadmin <1412244189@qq.com>
2018-11-12 22:25:51 +0800

    2018.11.12 日常提交

--- PHP/bin/competitor-info-submit.php
+++ PHP/bin/competitor-info-submit.php
@@ -1,6 +1,7 @@
 <?php
 
 include("../common/common-define.php");
+include("../common/common-functions.php");
 
 session_start();
 checkIsLegal(isset($_SESSION["compsname"]) && isset($_SESSION["username"]) && isset($_SESSION["authority"]), null, 1);
@@ -64,64 +65,5 @@ else {
 	checkIsLegal(false, $dbc, 9);
 }
 mysqli_close($dbc);
-
-function competitorInfoToArray($src) {
-	$groupinfo = preg_split("/(@g@)/", $src);
-	$group = array_values(array_filter($groupinfo, function($var) { return !($var & 1); }, ARRAY_FILTER_USE_KEY));
-	$info = array_values(array_filter($groupinfo, function($var) { return ($var & 1); }, ARRAY_FILTER_USE_KEY));
-
-	$solution = Array();
-	for ($i = 0; $i < count($group); $i++) {
-		$playerwork = preg_split("/(@r@)/", $info[$i]);
-		$player = array_values(array_filter($playerwork, function($var) { return !($var & 1); }, ARRAY_FILTER_USE_KEY));
-		$work = array_values(array_filter($playerwork, function($var) { return ($var & 1); }, ARRAY_FILTER_USE_KEY));
-		for ($j = 0; $j < count($player); $j++) {
-			array_push($solution, Array($player[$j], $work[$j], $group[$i]));
-		}
-	}
-
-	return $solution;
-}
-
-function competitorScoreToArray($src, $groupname, $scores) {
-	$groupinfo = preg_split("/(@g@)/", $src);
-	$group = array_values(array_filter($groupinfo, function($var) { return !($var & 1); }, ARRAY_FILTER_USE_KEY));
-	$info = array_values(array_filter($groupinfo, function($var) { return ($var & 1); }, ARRAY_FILTER_USE_KEY));
-
-	$isselected = false;
-	for ($i = 0; $i < count($group); $i++) {
-		if ($group[$i] == $groupname) {
-			$isselected = true;
-			$limitscore = preg_split("/(@r@)/", $info[$i]);
-			break;
-		}
-	}
-	if (!$isselected) { return false; }
-	
-	$limit = array_values(array_filter($limitscore, function($var) { return !($var & 1); }, ARRAY_FILTER_USE_KEY));
-	$score = array_values(array_filter($limitscore, function($var) { return ($var & 1); }, ARRAY_FILTER_USE_KEY));
-
-	$info_raw = preg_split("/(@c@|@r@)/", $scores);
-	$limit_raw = array_values(array_filter($info_raw, function($var) { return !($var & 1); }, ARRAY_FILTER_USE_KEY));
-	$score_raw = array_values(array_filter($info_raw, function($var) { return ($var & 1); }, ARRAY_FILTER_USE_KEY));
-
-	if ($limit != $limit_raw) { return false; }
-
-	for ($i = 0; $i < count($limit_raw); $i++) {
-		if($score_raw[$i] < preg_split("/(@s@)/", $score[$i])[0] || $score_raw[$i] > preg_split("/(@s@)/", $score[$i])[1]) {
-			return false;
-		}
-	}
-
-	return $score_raw;
-}
-
-function checkIsLegal($condition, $sqlconnection = null, $code) {
-	if (!$condition) {
-		echo "failed"/*."--code:".$code*/;
-		if (!is_null($sqlconnection)) { mysqli_close($sqlconnection); }
-		exit();
-	}
-}
 
 ?>
---
+++ files/ordinary-admins-management-page.php
@@ -0,0 +1,85 @@
+<?php
+
+include("../PHP/common/common-define.php");
+include("../PHP/common/common-functions.php");
+
+session_start();
+//checkIsLegal(isset($_POST["compsname"]), null, 1);
+$compsname = $_POST["compsname"];
+
+$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+$query = "SELECT  FROM competitor_score WHERE  comps_name = '$compsname'";
+$result = mysqli_query($dbc, $query);
+//checkIsLegal(mysqli_num_rows($result) > 0, $dbc, 2);
+
+$query = "SELECT competitor_info, score_rubric FROM comps_info WHERE comps_name = '$compsname'";
+$result = mysqli_query($dbc, $query);
+checkIsLegal(mysqli_num_rows($result) > 0, $dbc, 3);
+
+mysqli_data_seek($result, 0);
+$row = mysqli_fetch_array($result);
+$competitor_info = getGroupNameFromeCompetitorInfo($row[0]);
+array_push($competitor_info, "总计");
+$score_rubric = $row[1];
+
+?>
+
+<div class="ordinary-header">
+	<div class="compsname" id="compsname"><?php echo $compsname; ?></div>
+	<div class="header-line-continer"><div class="header-line"></div></div>
+</div>
+<div class="ordinary-body">
+	<div id="time" class="time">
+		<div class="ordinary-body-timer">
+			比赛倒计时
+			时分秒
+		</div>
+		<div style="margin: 0 auto; width: 300px; height: 2pc">
+			<input style="float: left" class="button" type="button" name="stratOrEnd" value="立即开始" />
+			<input style="float: right" class="button" type="button" name="stratOrEnd" value="立即结束" />
+		</div>
+	</div>
+	<div id="score" class="score">
+		<input class="flush" type="button" name="itemFlush" value="刷新" />
+		<div class="no-data" id="no-data">暂无数据</div>
+		<div style="width: 100%; border: 2px solid #BACDEE; z-index: 0; margin-top: 20px">
+			<div class="score-buttons" style="background: #BACDEE">
+				<ul id="groups">
+					<?php
+
+					foreach($competitor_info as $score_buttons) {
+						echo "<li onclick=\"clickGroups(this.id)\">$score_buttons</li>";
+					}
+
+					?>
+				</ul>
+			</div>
+			<div class="value">
+				<table id="score-value">
+					<th>
+						<td style="width: 40px;">编号</td>
+						<td style="width: 200px;">选手姓名</td>
+						<td style="width: 250px;">作品名称</td>
+						<div id="score-value-default"></div>
+						<td style="width: 200px;">总分</td>
+					</th>
+
+<?php
+
+// TODO: 
+
+?>
+
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="ordinary-footer"></div>
+
+<?php
+
+
+
+?>
